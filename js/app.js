4a917964817e1d0c22a041dc5dfd15357d032bbb
xaxcode <olsen.zach@gmail.com>
2015-02-01 21:04:03 -0700

    newest

--- js/app.js
+++ js/app.js
@@ -1,17 +1,92 @@
 
 $(document).ready(function(){
 	
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+	
+	/*--- Start Game ---*/
 
-  	});
+function newGame() {
+      randomNumber = newNumber();
+      console.log(randomNum);
 
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
 
+$( window ).load(function(){
+		var newNumber = Math.floor((Math.random() * 100) + 1);
+		console.log("New Number :" + newNumber);
+
+     return newNumber;
+} 
+
+
+
+$("#guessButton").click(function(){
+ 		var z = $("input#userGuess").val();
+ 		console.log("z :" + z);
+ 		if 		(z <= 100) {
+				$("#feedback").text(z);
+          clearText;
+          event,preventDefault();
+}
+
+
+
+  /*--- Guess Counter ---*/
+$("guessButton").click(function(){
+    var counter = 0;
+    z = $("#userGuess").val();
+    z = parseInt(z);
+    counter++;
+    console.log(counter);
+    $("span").text(counter);
+    clearText;
+    event.preventDefault();
+
+		var differenceNum = Math.abs(newNumber - z);
+    console.log(differenceNum);
+    if (z === newNumber) {
+        $("#feedback").text("Nailed It!");
+    }
+    else if (differenceNum >= 50) {
+       $("#feedback").text("freezing");
+ 		} 
+    else if (differenceNum >= 30) {
+       $("#feedback").text("warmer")
+    }
+    else (differenceNum >= 10) {
+        $("#feedback").text("hot!");
+    }
+}
+
+
+function guessCheck(z) {
+  if (isNan(z)) {
+    $("#feedback").text("use a number")
+  }
+}
+
+}); 
+
+}
+
+function clearText() {
+    $("userGuess").val('');
+}
+
+	/*--- New Game ---*/
+
+
+$(".new").click(function(){
+	event.preventDefault();
+   newGame();
 });
 
+    /*--- Display information modal box ---*/
+    $(".what").click(function(){
+      $(".overlay").fadeIn(1000);
 
+    });
+
+    /*--- Hide information modal box ---*/
+    $("a.close").click(function(){
+      $(".overlay").fadeOut(1000);
+    });
+});
