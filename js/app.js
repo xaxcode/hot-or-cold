4958fb7764fafaa000aa56354b4df370d94e2bee
xaxcode <olsen.zach@gmail.com>
2015-02-15 20:39:07 -0700

    done

--- js/app.js
+++ js/app.js
@@ -1,95 +1,68 @@
 
 $(document).ready(function(){
-	
-	
-	/*--- Start Game ---*/
-function newGame() {
-  randomNumber = generateNumber();
-  console.log(randomNumber);
 
-
-function generateNumber(){
-		var generateNumber = Math.floor((Math.random() * 100) + 1);
-		console.log("New Number :" + generateNumber);
-
-     return generateNumber;
-}
-
-
-/*
-$("#guessButton").click(function(){
- 		var z = $("input#userGuess").val();
- 		console.log("z :" + z);
- 		if 		(z <= 100) {
-				$("#feedback").text(z);
-        clearText;
-        event,preventDefault();
+    function newGame() {
+      $("#feedback").text("Make Your Guess!");
+      event.preventDefault();
+      $("#userGuess").val(" ");
+      ranNum = Math.floor((Math.random() * 100) + 1);
+      //count = 0;
+      $("#count").text(0);
+      count = 0;
     }
-}); 
-*/
+ 
+  var ranNum = Math.floor((Math.random() * 100) + 1);
+  var count = 0;
+  var guess;
 
-
-function clearText() {
-    $("userGuess").val('');
-}
-
-  /*--- Guess Counter ---*/
-$("guessButton").click(function(){
-    var counter = 0;
-    z = $("#userGuess").val();
-    z = parseInt(z);
-    counter++;
-    console.log(counter);
-    $("span").text(counter);
-    clearText;
+  $('#guessButton').click(function(){
     event.preventDefault();
+    count++;
+    $("#count").text(count);
+       console.log(ranNum);
+     guess = $('#userGuess').val();
+     guess = parseInt(guess);
+     var compareNum = Math.abs(guess-ranNum);
+       console.log(compareNum);
 
-		var differenceNum = Math.abs(generateNumber - z);
-    console.log(differenceNum);
-    if (z === generateNumber) {
-        $("#feedback").text("Nailed It!");
+    if ( compareNum == 0 ) {
+     $("#feedback").text("Nailed It!");
+    } 
+    else if (compareNum >= 50) {
+     $('#feedback').text("Ice cold!");
     }
-    else if (differenceNum >= 50) {
-       $("#feedback").text("freezing");
- 		} 
-    else if (differenceNum >= 30) {
-       $("#feedback").text("warmer")
+    else if (compareNum >= 30 && compareNum < 50) {
+     $('#feedback').text("Cold!");
     }
-    else { (differenceNum >= 10) 
-        $("#feedback").text("hot!");
-}
-
-
-function guessCheck(z) {
-  if (isNan(z)) {
-    $("#feedback").text("use a number")
-  }
-  else {
-    return z;
-  }
-}
-
-}); 
-
-
-/*--- New Game ---*/
-
-
-$(".new").click(function(){
-	event.preventDefault();
-   newGame();
-}); 
+    else if ( compareNum >= 20 && compareNum < 30) {
+     $('#feedback').text("Warm");
+    }
+    else if (compareNum >= 10 && compareNum < 20) {
+     $('#feedback').text("Hot");
+    }
+    else if (compareNum >= 1 && compareNum < 10){
+     $('#feedback').text("Very Hot!");
+    }
+    else {
+     $('#feedback').text("Enter A Number");
+    }      
+            
+  }); 
+   
+    
+  
+  $('.new').click(function(){
+    newGame();
+  });  
 
     /*--- Display information modal box ---*/
-    $(".what").click(function(){
-      $(".overlay").fadeIn(1000);
+      $(".what").click(function(){
+        $(".overlay").fadeIn(1000);
+      });
 
-    });
+      /*--- Hide information modal box ---*/
+      $("a.close").click(function(){
+        $(".overlay").fadeOut(1000);
+      });
 
-    /*--- Hide information modal box ---*/
-    $("a.close").click(function(){
-      $(".overlay").fadeOut(1000);
-    });
-}); 
-
-
+});
